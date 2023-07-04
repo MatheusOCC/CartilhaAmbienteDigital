c7ecd5fb42ecd07d7a943fdd65a100886aba6864
Matheus Cerqueira <escrevaparamatheus@gmail.com>
2023-07-03 22:22:04 -0300

    Ajuste de referências
    Complementares ajustados e referencias migradas para cartilhas.py. Também adiciona nome à página

--- extras.py
+++ extras.py
@@ -8,66 +8,28 @@ def carrega_extras():
 
     st.title('Materiais Recomendados')
 
-
-    st.header('Referências Utilizadas')
-    with st.expander('Referências Utilizadas'):
-        st.write('FIA (2023) "Cyberbullying: o que é, consequências e dados no Brasil", https://fia.com.br/blog/cyberbullying/, Janeiro.')
-        st.write('BRASIL ESCOLA (?) "Cyberbullying", https://brasilescola.uol.com.br/sociologia/cyberbullying.htm')
-        st.write('OLHAR DIGITAL (2022) "Quais são os sinais de um perfil falso nas redes sociais?", https://olhardigital.com.br/2022/04/29/tira-duvidas/sinais-perfil-falso-redes-sociais/, Abril.')
-        st.write('TILT (2022) "Descubra se um perfil é fake no Instagram, Facebook, WhatsApp e TikTok", https://www.uol.com.br/tilt/noticias/redacao/2022/04/26/como-saber-se-perfil-e-fake-no-instagram-facebook-whatsapp-e-tiktok.htm, Abril.')
-        st.write('KAPERSKY (?) "Bullying virtual: O crime virtual do século", https://www.kaspersky.com.br/resource-center/preemptive-safety/cyberbullying-and-cybercrime/')
-        st.write('GOVERNO DO BRASIL (2022) "90% dos lares brasileiros já tem acesso à internet no Brasil, aponta pesquisa", https://www.gov.br/casacivil/pt-br/assuntos/noticias/2022/setembro/90-dos-lares-brasileiros-ja-tem-acesso-a-internet-no-brasil-aponta-pesquisa, Outubro.')
-        st.write('G1 (2016) "Software Livre: vale a pena ser substituído por programas pagos?", https://g1.globo.com/tecnologia/blog/tira-duvidas-de-tecnologia/post/software-livre-vale-pena-ser-substituido-por-programas-pagos.html, Novembro.')
-        st.write('KAPERSKY (?) "Como evitar riscos à segurança em redes Wi-Fi públicas", https://www.kaspersky.com.br/resource-center/preemptive-safety/public-wifi-risks')
-        st.write('KAPERSKY (?) "Centro de recursos", https://www.kaspersky.com.br/resource-center')
-        st.write('DANRESA (2023) "Os Riscos do Uso de WiFi em Aeroportos e Como se Proteger", https://www.danresa.com.br/danresa-portal/ciberseguranca/os-riscos-do-uso-de-wifi-em-aeroportos-e-como-se-proteger/, Maio.')
-        st.write('TECMUNDO (2013) "Quais os riscos de usar um WiFi público?", https://www.tecmundo.com.br/wi-fi/39348-quais-os-riscos-de-usar-um-wifi-publico-.htm, Maio.')
-        st.write('FORBES (2020) "Cibersegurança: entenda os perigos do ambiente digital", https://forbes.com.br/forbes-tech/2020/11/ciberseguranca-entenda-os-perigos-do-ambiente-digital/, Novembro.')
-        st.write('G1 (2023) "Ex-BBB recebe ataques nas redes sociais após fazer apelo para encontrar mãe desaparecida; caso foi registrado na polícia há 2 meses", https://g1.globo.com/ba/bahia/noticia/2023/06/26/ex-bbb-recebe-ataques-nas-redes-sociais-apos-fazer-apelo-para-encontrar-mae-desaparecida-caso-foi-registrado-na-policia-ha-2-meses.ghtml, Junho.')
-        st.write('AGÊNCIA BRASIL (2020) “Pesquisa mostra exclusão de idosos do mundo digital e da escrita”,https://agenciabrasil.ebc.com.br/geral/noticia/2020-08/pesquisa-mostra-exclusao-de-idosos-do-mundo-digital-e-da-escrita, Agosto.')
-        st.write('AGÊNCIA BRASIL (2021) “Estudo mostra que pandemia intensificou uso das tecnologias digitais”, https://agenciabrasil.ebc.com.br/geral/noticia/2021-11/estudo-mostra-que-pandemia-intensificou-uso-das-tecnologias-digitais, Novembro.')
-        st.write('BBC NEWS BRASIL (2021) “Nativos digitais não sabem buscar conhecimento na internet, diz OCDE”, https://www.bbc.com/portuguese/geral-57286155, Maio.')
-        st.write('CERT (?) “Cartilha de Segurança Para a Internet”, https://cartilha.cert.br/fasciculos/.')
-        st.write('CGIBR (2012) “Cartilha de Segurança para a Internet”, https://www.cgi.br/publicacao/cartilha-de-seguranca-para-internet/.')
-        st.write('CYBERTECH BRASIL (2022) “Cartilha CyberTech Brasil de conscientização em segurança”, https://cybertechbr.org/baixe-ja-nossa-cartilha-cybertech-brasil-de-conscientizacao-em-seguranca/')
-        st.write('ESTADO DE MINAS (2023) “Discord na mira: coação, chantagem e violência entre jovens”, https://www.em.com.br/app/noticia/tecnologia/2023/06/28/interna_tecnologia,1513306/discord-na-mira-coacao-chantagem-e-violencia-entre-jovens.shtml, Junho.')
-        st.write('FEBRABAN (2021) “Medo, ganância e desconhecimento são iscas para golpes digitais, alertam especialistas”, https://febrabantech.febraban.org.br/videos/medo-ganancia-e-desconhecimento-sao-iscas-para-golpes-digitais-alertam-especialistas, Fevereiro.')
-        st.write('G1 (2022) “Tentativas de golpes por meios digitais cresceram 20% no segundo trimestre deste ano no Brasil”, https://g1.globo.com/tecnologia/noticia/2022/10/11/tentativas-de-golpes-por-meios-digitais-cresceram-20percent-no-segundo-trimestre-deste-ano-no-brasil.ghtml, Outubro.')
-        st.write('MINISTÉRIO DA EDUCAÇÃO (2023) “ESCOLA SEGURA - Como lidar com conteúdos de violência online e conversar com crianças e jovens sobre o tema”, https://www.gov.br/mec/pt-br/assuntos/noticias/mec-lanca-cartilha-para-enfrentamento-a-violencia-on-line.')
-        st.write('MINISTERIO PÚBLICO DFT (2015) “Cartilha Orientativa Ética e Segurança Digital”, https://www.mpdft.mp.br/portal/pdf/imprensa/cartilhas/Cartilha_Orientativa_Etica_Seguranca_Digital_MPDFT.pdf.')
-        st.write('OAB-SP (2023) “Segurança da Informação - Cartilha de Boas Práticas para Advogados”, https://jornaldaadvocacia.oabsp.org.br/wp-content/uploads/2023/04/cartilha-boas-praticas-virtuais-oab-sp.pdf')
-        st.write('PLANALTO (2014) “Cartilha da Segurança - Simples Atitudes que Podem Evitar Grandes Problemas”, http://www4.planalto.gov.br/cgd/assuntos/publicacoes/campanha-de-sensibilizacao-em-sic-2014/cartilha-de-seguranca-da-informacao.pdf.')
-        st.write('SAFERNET (2015) “Diálogo Virtual 2.0: Preocupado com o que acontece na Internet? Quer Conversar?”, https://new.safernet.org.br/sites/default/files/content_files/Di%C3%A1logo_Virtual_Low_Web_SN_Unicef_PFDC_CGI.pdf.')
-        st.write('UFRGS (2021) “Cartilha de Segurança da Informação”, https://www.ufrgs.br/cpd/wp-content/uploads/2021/05/CartilhaSegurancaInformacao.pdf')
-        st.write('WORLD HEALTH ORGANIZATION (2020) “WHO Director-Generals opening remarks at the media briefing on COVID-19 - 11 March 2020”, https://www.who.int/director-general/speeches/detail/who-director-general-s-opening-remarks-at-the-media-briefing-on-covid-19---11-march-2020 , Março.')
-        st.write('UNICEF (2019)	“Pesquisa do UNICEF: Mais de um terço dos jovens em 30 países relatam ser vítimas de bullying online”	https://www.unicef.org/brazil/comunicados-de-imprensa/mais-de-um-terco-dos-jovens-em-30-paises-relatam-ser-vitimas-bullying-online')
-        st.write('SAFERNET (?) “Canal de Denúncia”, https://new.safernet.org.br/denuncie')
-
-        
-
-
     st.header('Indicações de Leitura')
     with st.expander('Indicações de Leitura'):
-        st.write('https://www.perallis.com/news (Empresa especializada em cibersegurança)')
-        st.write('https://www.olharldigital.com/ (Empresa especializada em cibersegurança)')
-        st.write('https://www.tecmundo.com.br/seguranca (Empresa especializada em tecnologia, com notícias de cibersegurança)')
-        st.write('https://lupa.uol.com.br/ (Agência de notícias com foco em desinformação)')
-        st.write('https://www.unicef.org/brazil/cyberbullying-o-que-eh-e-como-para-lo (Como parar com o Bullying)')
-        st.write('https://help.twitter.com/pt/safety-and-security/report-abusive-behavior#:~:text=Pressione%20e%20mantenha%20pressionada%20a,e%20selecione%20Denunciar%20%40username).&text=Caso%20voc%C3%AA%20selecione%20%C3%89%20abusivo,o%20problema%20que%20est%C3%A1%20denunciando (Como denunciar abuso no Twitter)')
+        st.write('Empresa especializada em cibersegurança - https://www.perallis.com/news')
+        st.write('Empresa especializada em cibersegurança - https://www.olharldigital.com/')
+        st.write('Empresa especializada em tecnologia, com notícias de cibersegurança - https://www.tecmundo.com.br/seguranca')
+        st.write('Agência de notícias com foco em desinformação - https://lupa.uol.com.br/')
+        st.write('Como parar com o Bullying - https://www.unicef.org/brazil/cyberbullying-o-que-eh-e-como-para-lo')
+        st.write('Como denunciar abuso no Twitter- https://help.twitter.com/pt/safety-and-security/report-abusive-behavior#:~:text=Pressione%20e%20mantenha%20pressionada%20a,e%20selecione%20Denunciar%20%40username).&text=Caso%20voc%C3%AA%20selecione%20%C3%89%20abusivo,o%20problema%20que%20est%C3%A1%20denunciando')
 
     with st.expander('Notícias Importantes'):
-        st.write('https://canaltech.com.br/hacker/hackers-chineses-invadem-entidades-governamentais-pelo-mundo-brasil-na-lista-158443/')
-        st.write('https://vejario.abril.com.br/coluna/fabio-barbirato/como-evitar-que-seu-filho-seja-vitima-de-crimes-na-internet/')
-        st.write('https://www.adrenaline.com.br/seguranca/windows-11-coleta-e-envia-dados-de-usuarios-para-varias-empresas-denuncia-youtuber/')
-        st.write('https://olhardigital.com.br/2019/08/09/dicas-e-tutoriais/como-dificultar-a-coleta-de-dados-no-computador-e-no-celular/')
-        st.write('https://www.techtudo.com.br/noticias/2022/03/golpe-do-perfil-fake-em-relacoes-virtuais-atinge-1-a-cada-4-brasileiros.ghtml')
-        st.write('https://www.bbc.com/portuguese/brasil-42172146')
-        st.write('https://arquivo.canaltech.com.br/comportamento/pesquisa-da-intel-revela-dados-sobre-cyberbullying-no-brasil-46105/')
-        st.write('https://www.uol.com.br/splash/noticias/2022/10/11/o-caso-real-de-linchamento-que-inspirou-gloria-perez-em-travessia.html')
+        st.write('Hackers Chineses Invadem Entidades Governamentais pelo Mundo : Brasil na Lista - https://canaltech.com.br/hacker/hackers-chineses-invadem-entidades-governamentais-pelo-mundo-brasil-na-lista-158443/')
+        st.write('Como Evitar que seu Filho seja Vítima de Crimes na Internet - https://vejario.abril.com.br/coluna/fabio-barbirato/como-evitar-que-seu-filho-seja-vitima-de-crimes-na-internet/')
+        st.write('Windows 11 coleta e envia dados de usuários para várias Empresas, Denuncia youtuber - https://www.adrenaline.com.br/seguranca/windows-11-coleta-e-envia-dados-de-usuarios-para-varias-empresas-denuncia-youtuber/')
+        st.write('Como dificultar a coleta de dados no computador e no celular - https://olhardigital.com.br/2019/08/09/dicas-e-tutoriais/como-dificultar-a-coleta-de-dados-no-computador-e-no-celular/')
+        st.write('Golpe de Perfil Fake em relações virtuais atinge 1 a cada 4 brasileiros - https://www.techtudo.com.br/noticias/2022/03/golpe-do-perfil-fake-em-relacoes-virtuais-atinge-1-a-cada-4-brasileiros.ghtml')
+        st.write('Exclusivo: investigação revela exército de perfis falsos usados para influenciar eleições no Brasil - https://www.bbc.com/portuguese/brasil-42172146')
+        st.write('Pesquisa da Intel revela dados sobre Cyberbullying no brasil - https://arquivo.canaltech.com.br/comportamento/pesquisa-da-intel-revela-dados-sobre-cyberbullying-no-brasil-46105/')
+        st.write(' Caso real de linchamento que inspirou Glória Perez em Travessia - https://www.uol.com.br/splash/noticias/2022/10/11/o-caso-real-de-linchamento-que-inspirou-gloria-perez-em-travessia.html')
     with st.expander('Explicando conceitos'):
         st.subheader('Software Livre vs proprietário')
-        st.write('https://g1.globo.com/tecnologia/blog/tira-duvidas-de-tecnologia/post/software-livre-vale-pena-ser-substituido-por-programas-pagos.html')
-        st.write('https://www.gnu.org/philosophy/free-sw.pt-br.html (Definição original de Software livre)')
+        st.write('Comparação Software Livre e Programas Pagos - https://g1.globo.com/tecnologia/blog/tira-duvidas-de-tecnologia/post/software-livre-vale-pena-ser-substituido-por-programas-pagos.html')
+        st.write('Definição original de Software livre - https://www.gnu.org/philosophy/free-sw.pt-br.html')
 
 
     st.header('Indicações de Vídeos')
@@ -82,11 +44,12 @@ def carrega_extras():
 
     st.header('Boas práticas')
     with st.expander('Ferramentas Úteis'):
-        st.write('https://www.haveibeenpwned.com/ (Site que verifica se algum endereço de e-mail ou senha seus já foi vazado)')
-        st.write('https://tosdr.org/pt_BR/frontpage/ (Site comunitário que analisa os termos de serviços de sites e aplicativos no mundo)')
-        st.write('https://www.boatos.org/ e https://www.e-farsas.com/ (Sites que checam a veracidade de boatos viralizados recentemente)')
-        st.write('https://www.techtudo.com.br/dicas-e-tutoriais/2023/03/como-ativar-monitoramento-parental-nas-redes-sociais-e-proteger-seu-filho-edapps.ghtml (Site ensinando como ativar o monitoramento parental)')
+        st.write('Site que verifica se algum endereço de e-mail ou senha seus já foi vazado - https://www.haveibeenpwned.com/')
+        st.write('Site comunitário que analisa os termos de serviços de sites e aplicativos no mundo - https://tosdr.org/pt_BR/frontpage/')
+        st.write('Sites que checam a veracidade de boatos viralizados recentemente - https://www.boatos.org/ e https://www.e-farsas.com/')
+        st.write('Site ensinando como ativar o monitoramento parental - https://www.techtudo.com.br/dicas-e-tutoriais/2023/03/como-ativar-monitoramento-parental-nas-redes-sociais-e-proteger-seu-filho-edapps.ghtml')
         
-    st.header('Lista de Delegacias de cibercrimes')
-    with st.expander('Delegacias do Brasil'):
-        st.write('https://new.safernet.org.br/content/delegacias-cibercrimes (Lista de todas as delegacias do Brasil)') 
+    st.header('Ajuda / Denúncia')
+    with st.expander('Ajuda / Denúncia'):
+        st.write('Linha de Ajuda (helpline) - https://new.safernet.org.br/helpline?gclid=Cj0KCQjwnf-kBhCnARIsAFlg491cAxCfJSYZiW4XY0cQvhDGyaohOFFyzz_hHFndxNKSWez4knI0mhQaAq5BEALw_wcB#') 
+        st.write('Lista de todas as delegacias do Brasil - https://new.safernet.org.br/content/delegacias-cibercrimes') 
--- main.py
+++ main.py
@@ -4,6 +4,8 @@ import logging
 import cartilhas
 import extras
 
+st.set_page_config(page_title='Cartilha: Segurança e Privacidade no Ambiente Digital')
+
 st.title('Cartilha: Segurança e Privacidade no Ambiente Digital')
 st.caption('Desenvolvido para a disciplina Computador, Ética e Sociedade (MATA68) da UFBA - Universidade Federal da Bahia, em 2023.1')
 st.caption('Autores: Indaiara Bonfim; Jade do Nascimento; Marcos dos Anjos; Matheus Cerqueira; Rômulo Chamusca')
